b18b249449a9cfacef103c49f2bac337f1b14e4b
gurdian0614 <ivan.gurdian@outlook.com>
2024-07-19 20:44:33 -0600

    Implementacion

--- HerenciaQ32024/Program.cs
+++ HerenciaQ32024/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HerenciaQ32024;
+
+string tienePatas, esDomestico, esHerbivoro, esCarnivoro;
+
+Vertebrado vertebrado = new Vertebrado();
+Console.WriteLine("Ingrese el nombre:");
+vertebrado.Nombre = Console.ReadLine();
+Console.WriteLine("Ingrese el color:");
+vertebrado.Color = Console.ReadLine();
+Console.WriteLine("Ingrese el tamaño:");
+vertebrado.Tamano = Console.ReadLine();
+Console.WriteLine("Ingrese la Familia perteneciente del animal:");
+vertebrado.Familia = Console.ReadLine();
+Console.WriteLine("Ingrese el tipo de animal:");
+vertebrado.Tipo = Console.ReadLine();
+Console.WriteLine("¿El animal tiene patas? (s/n)");
+tienePatas = Console.ReadLine();
+Console.WriteLine("¿El animal es doméstico? (s/n)");
+esDomestico = Console.ReadLine();
+Console.WriteLine("¿El animal es herbívoro? (s/n)");
+esHerbivoro = Console.ReadLine();
+
+if (tienePatas == "s")
+{
+    vertebrado.TienePatas = true;
+}
+
+if (esDomestico == "s")
+{
+    vertebrado.EsDomestico = true;
+}
+
+if (esHerbivoro == "s")
+{
+    vertebrado.EsHerbivoro = true;
+    vertebrado.EsCarnivoro = false;
+} else
+{
+    vertebrado.EsHerbivoro = false;
+    vertebrado.EsCarnivoro = true;
+}
+
+vertebrado.Imprimir();
