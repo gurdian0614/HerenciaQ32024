01d5ee005255a6245b61116b1739734409a2faf7
gurdian0614 <ivan.gurdian@outlook.com>
2024-07-19 20:44:20 -0600

    Crear clase hija

---
+++ HerenciaQ32024/Vertebrado.cs
@@ -0,0 +1,30 @@
+﻿
+namespace HerenciaQ32024
+{
+    public class Vertebrado : Animal
+    {
+        public string Tipo { get; set; }
+
+        public bool TienePatas { get; set; }
+
+        public bool EsDomestico { get; set; }
+
+        public bool EsHerbivoro { get; set; }
+
+        public bool EsCarnivoro { get; set; }
+
+        public void Imprimir()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Tamaño: {Tamano}");
+            Console.WriteLine($"Familia: {Familia}");
+            Console.WriteLine($"Tipo: {Tipo}");
+            Console.WriteLine($"Tiene Patas: {(TienePatas ? "Si" : "No")}");
+            Console.WriteLine($"Es Doméstico: {(EsDomestico ? "Si" : "No")}");
+            Console.WriteLine($"Es Herbívoro: {(EsHerbivoro ? "Si" : "No")}");
+            Console.WriteLine($"Es Carnívoro: {(EsCarnivoro ? "Si" : "No")}");
+        }
+    }
+}
