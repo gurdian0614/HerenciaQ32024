35d481a79cb7f8807534fe471ac8afae85ca26e9
gurdian0614 <ivan.gurdian@outlook.com>
2024-07-19 20:44:13 -0600

    Crear clase padre

---
+++ HerenciaQ32024/Animal.cs
@@ -0,0 +1,26 @@
+﻿
+namespace HerenciaQ32024
+{
+    public class Animal
+    {
+        /// <summary>
+        /// Nombre del Animal
+        /// </summary>
+        public string Nombre { get; set; }
+
+        /// <summary>
+        /// Color del Animal
+        /// </summary>
+        public string Color { get; set; }
+
+        /// <summary>
+        /// Tamaño del Animal
+        /// </summary>
+        public string Tamano { get; set; }
+
+        /// <summary>
+        /// Familia a la que pertenece el Animal
+        /// </summary>
+        public string Familia { get; set; }
+    }
+}
